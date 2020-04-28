8698b0235c739ab9059290fcf18620b80b34a66c
CarolGF <karola_gf@hotmail.com>
2020-04-28 17:14:57 +0100

    Updates

---
+++ precache-manifest.9fd72d94f9d1a07e9912d56ea8e8dd00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3995589cd56567219080ff5c7f69d28",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "2ccc703ce2165380b90f",
+    "url": "/pomodoro-clock/static/css/main.d96fc106.chunk.css"
+  },
+  {
+    "revision": "28e94583b1a1ce046a80",
+    "url": "/pomodoro-clock/static/js/2.00e7c34a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.00e7c34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ccc703ce2165380b90f",
+    "url": "/pomodoro-clock/static/js/main.ab5c6f16.chunk.js"
+  },
+  {
+    "revision": "d049ad462579bdd11957",
+    "url": "/pomodoro-clock/static/js/runtime-main.6103215c.js"
+  }
+]);
